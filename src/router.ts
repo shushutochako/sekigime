6f35a06e79a3f4098b9df82331b73f7c7190270b
Nobuyasu Masujima <shushutochako@gmail.com>
2018-12-14 23:57:44 +0900

    feat: modify layout

--- src/router.ts
+++ src/router.ts
@@ -2,8 +2,11 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
 import CreateTables from './views/CreateTables.vue';
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(Router);
+Vue.use(ElementUI)
 
 export default new Router({
   mode: 'history',
--- src/views/CreateTables.vue
+++ src/views/CreateTables.vue
@@ -1,30 +1,44 @@
 <template>
-  <div class="create-tables">
-    <div class="table-setting">
-        <input type="number" tabindex="1" class="table-setting-item" name="numberOfPerTable" id="number-of-per-table" v-model="numberOfPerTables"/>
-    </div>
-    <div class="person-area">
-      <div class="input-area">
-        <input type="text" tabindex="1" name="person-name" id="person-name" v-model="personName">
-        <button @click="onAdd">add!</button>
-      </div>
-      <div v-for="(person, i) in getPersons()" :key="i">{{person.name}}</div>
-    </div>
-    <div class="table-container">
-      <my-table v-for="(table, i) in tables" :table="table" :key="i"/>
-    </div>
-    <button @click="onShuffle">席を決める</button>
-  </div>
+  <el-container style="height: 500px; border: 1px solid #eee">
+    <el-aside width="300px" style="background-color: rgb(238, 241, 246)">
+      <el-form :inline="true" :model="sizeForm" size="mini">
+        <el-form-item label="1チームの人数：">
+          <el-input type="number" tabindex="1" id="number-of-per-table" v-model="numberOfPerTables"></el-input>
+        </el-form-item>
+      </el-form>
+      <el-form :inline="true">
+        <el-form-item label="名前：">
+          <el-input
+            type="text"
+            tabindex="1"
+            name="person-name"
+            id="person-name"
+            v-model="personName"
+          ></el-input>
+        </el-form-item>
+        <el-button @click="onAdd">追加</el-button>
+        <div v-for="(person, i) in getPersons()" :key="i">{{person.name}}</div>
+      </el-form>
+    </el-aside>
+    <el-container>
+      <el-main>
+        <div class="table-container">
+          <my-table v-for="(table, i) in tables" :table="table" :key="i"/>
+        </div>
+        <button @click="onShuffle">席を決める</button>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script lang="ts">
-import { mapGetters, mapMutations, createNamespacedHelpers } from 'vuex'
-import { Mutation, Getter } from 'vuex-class'
+import { mapGetters, mapMutations, createNamespacedHelpers } from "vuex";
+import { Mutation, Getter } from "vuex-class";
 import { Component, Prop, Vue } from "vue-property-decorator";
 import MyTable from "@/components/Table.vue";
 import TableEntity from "@/entity/Table.ts";
 import Person from "@/entity/Person";
-import Table from '@/components/Table.vue';
+import Table from "@/components/Table.vue";
 
 @Component({
   components: {
@@ -37,10 +51,13 @@ import Table from '@/components/Table.vue';
 export default class Createtable extends Vue {
   private tables: Array<TableEntity> = [];
   private personName: string = "";
-  @Mutation('TableSetting/updateNumberOfPerTables') updateNumberOfPerTables!: (newValue:number) => void
-  @Mutation('Persons/add') addPerson!: (newPerson:any) => void
-  @Getter('Persons/getPersons') getPersons!: () => [any]
-  @Getter('TableSetting/getNumberOfPerTables') getNumberOfPerTables!: () => number
+  @Mutation("TableSetting/updateNumberOfPerTables") updateNumberOfPerTables!: (
+    newValue: number
+  ) => void;
+  @Mutation("Persons/add") addPerson!: (newPerson: any) => void;
+  @Getter("Persons/getPersons") getPersons!: () => [any];
+  @Getter("TableSetting/getNumberOfPerTables")
+  getNumberOfPerTables!: () => number;
 
   mounted(): void {
     this.dummy();
@@ -72,13 +89,13 @@ export default class Createtable extends Vue {
   }
 
   onClick(): void {
-    console.log(this.getNumberOfPerTables())
+    console.log(this.getNumberOfPerTables());
   }
 
   onAdd(): void {
     this.addPerson({
       name: this.personName
-    })
+    });
   }
 
   private onShuffle(): void {
@@ -89,16 +106,16 @@ export default class Createtable extends Vue {
     const perTable = this.getNumberOfPerTables();
     const members: Array<Person> = this.getPersons().concat();
     const personCount = members.length;
-    const tableCount =  Math.floor(personCount / perTable);
+    const tableCount = Math.floor(personCount / perTable);
 
     const tables = Array<TableEntity>();
-    tableLoop : for (let i = 0; i < tableCount; i++ ) {
+    tableLoop: for (let i = 0; i < tableCount; i++) {
       const persons = Array<Person>();
       for (let j = 0; j < perTable; j++) {
         if (members.length < 1) {
           break tableLoop;
         }
-        const rand = Math.floor(Math.random() * (members.length));
+        const rand = Math.floor(Math.random() * members.length);
         persons.push(members.splice(rand, 1)[0]);
       }
 
@@ -109,7 +126,10 @@ export default class Createtable extends Vue {
   }
 
   // 余りメンバーの割り当て
-  assignSurplus(tables: Array<TableEntity>, members: Array<Person>): Array<TableEntity> {
+  assignSurplus(
+    tables: Array<TableEntity>,
+    members: Array<Person>
+  ): Array<TableEntity> {
     const surplus = members.length;
 
     const assigns: Array<number> = [];
@@ -121,13 +141,12 @@ export default class Createtable extends Vue {
       assigns.push(rand);
       return rand;
     };
-    for (let i = 0; i < surplus; i++ ) {
+    for (let i = 0; i < surplus; i++) {
       const select = lottery(surplus);
-      tables[select].members.push(members.splice(0,1)[0]);
+      tables[select].members.push(members.splice(0, 1)[0]);
     }
     return tables;
   }
-
 }
 </script>
 
@@ -141,14 +160,14 @@ export default class Createtable extends Vue {
 }
 .person-area {
   margin: 10px;
-  min-height:200px;
+  min-height: 200px;
   background: lightgray;
   display: -webkit-flex;
   display: flex;
   -webkit-flex-direction: column; /* Safari */
   flex-direction: column;
 }
-.person-area .input-area{
+.person-area .input-area {
   display: -webkit-flex;
   justify-content: flex-end;
 }
