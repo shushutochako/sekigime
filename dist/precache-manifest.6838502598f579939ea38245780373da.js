5239f484ed98da68e6df80c01c564e502fd64216
Nobuyasu Masujima <shushutochako@gmail.com>
2018-12-16 22:24:22 +0900

    feat: modify for heroku

---
+++ dist/precache-manifest.6838502598f579939ea38245780373da.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "ebdd8c61c5961eb653c7",
+    "url": "/js/chunk-vendors.6a226de8.js"
+  },
+  {
+    "revision": "d16725ea81af41524b10",
+    "url": "/js/app.6885b760.js"
+  },
+  {
+    "revision": "e7c2b3851ab3965b1ed8",
+    "url": "/js/about.b8cabc56.js"
+  },
+  {
+    "revision": "b31209bed1363ba8c3b3adc35e170363",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "ebdd8c61c5961eb653c7",
+    "url": "/css/chunk-vendors.43d1c9b4.css"
+  },
+  {
+    "revision": "d16725ea81af41524b10",
+    "url": "/css/app.5e7d619f.css"
+  }
+];
