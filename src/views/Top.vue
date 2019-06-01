f9a91a724e7daa361725c5c3fa5cb903608f2f86
Nobuyasu Masujima <shushutochako@gmail.com>
2019-06-01 13:23:56 +0900

    feat: add description

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -5,8 +5,8 @@
         <common-header></common-header>
       </el-header>
       <div class="heading">
-        <h2>面倒なチーム分けを自動で作成します。共有URLで他の人に簡単に共有!</h2>
-        <p class="note">※ 作成したチーム表の保存や画像ダウンロードも可能です。</p>
+        <h2 class="main-heading">面倒なチーム分けを自動で作成します。共有URLで他の人に簡単に共有!</h2>
+        <br>
       </div>
       <el-main class="main input-container">
         <el-form
@@ -24,8 +24,31 @@
         </el-form>
       </el-main>
     </el-container>
+    <div class="description">
+      <h3>使い方</h3>
+      <ol>
+        <li>イベントを作成</li>
+        <li>参加メンバーを追加</li>
+        <li>「チームを決める」でチームを自動編成</li>
+        <li>ドラッグ&ドロップでチーム編成を調整</li>
+        <li>データを共有したい場合は保存ボタンで保存</li>
+        <li>チーム表をダウンロードしたい場合は「チーム表をダウンロード」</li>
+        <li>URLで他の人とデータを共有</li>
+      </ol>
+      <h3>シチュエーション</h3>
+      <ul>
+        <li>飲み会の席分けを決めたい</li>
+        <li>イベントのグループ分けを決めたい</li>
+        <li>チームランチの編成を作りたい</li>
+      </ul>
+    </div>
     <div class="tweet-container">
-      <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" data-size="large" class="twitter-share-button" data-show-count="false">Tweet</a>
+      <a
+        href="https://twitter.com/share?ref_src=twsrc%5Etfw"
+        data-size="large"
+        class="twitter-share-button"
+        data-show-count="false"
+      >Tweet</a>
     </div>
   </div>
 </template>
@@ -109,14 +132,25 @@ export default class Createtable extends Vue {
   width: 200px;
 }
 
+.main-heading {
+  color: darkorange;
+}
 .heading {
   margin-top: 10px;
   margin-left: 100px;
   margin-right: 100px;
   text-align: left;
-  color: darkorange;
   border-bottom: medium solid #ececec;
 }
+
+.description {
+  margin-top: 10px;
+  margin-left: 100px;
+  margin-right: 100px;
+  text-align: left;
+  border-bottom: medium solid #ececec;
+}
+
 .note {
   margin-top: 10px;
   align: left;
